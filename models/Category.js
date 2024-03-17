a7bae1d356dc0c63acf1f17035e93433eaa54174
Jetnik Syla <sjetnik@gmail.com>
2024-03-17 15:53:31 -0400

    Define type and rules for the  data to be stored in each column

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const { UUIDV4, Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection");
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    // Define type and rules for the  data to be stored in each column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
